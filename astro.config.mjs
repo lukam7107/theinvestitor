9e71ad6da0cfd1e06894505f4ad141837c5c7ce1
Luka Marincek <luka.marincek7@gmail.com>
2026-09-08 22:52:21 +0200

    Dodan sitemap.xml (@astrojs/sitemap) + robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://theinvestitor.com',
   output: 'hybrid',
   adapter: vercel(),
+  integrations: [sitemap()],
 });
